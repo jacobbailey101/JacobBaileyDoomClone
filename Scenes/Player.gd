3d7f867853727462a8d9a0b40a57cf8ffde71604
jacobbailey101 <88062523+jacobbailey101@users.noreply.github.com>
2022-06-20 13:03:04 +1200

    amo
    fixed amo

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -7,7 +7,12 @@ var max_speed = 8
 var mouse_sensitivity = 0.002
 
 #gun_variables
-
+onready var pistol = preload("res://Scenes/Pistol.tscn")
+onready var shotgun = preload("res://Scenes/shotgun.tscn")
+onready var uzi = preload("res://Scenes/Uzi.tscn")
+onready var rocketlauncher = preload("res://Scenes/RocketLauncher.tscn")
+var current_gun = 0
+onready var carried_guns = [pistol,shotgun,uzi,rocketlauncher]
 
 #functions
 func _ready():
@@ -27,21 +32,39 @@ func get_input():
 	return input_dir
 	
 func _unhandled_input(event):
-	pass
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * mouse_sensitivity)
+		$Pivot.rotate_x(-event.relative.y * mouse_sensitivity)
+		$Pivot.rotation.x = clamp($Pivot.rotation.x,-1.2,1.2)
 	
 func _physics_process(delta):
 	#gravity
 	velocity.y += gravity * delta
 	var desired_velocity = get_input() * max_speed
 	velocity.x = desired_velocity.x
-	velocity.z =desired_velocity.z
+	velocity.z = desired_velocity.z
 	velocity = move_and_slide(velocity, Vector3.UP, true)
 	
+
+	
 func change_gun(gun):
-	pass
+	$Pivot/Gun.get_child(0).queue_free()
+	var new_gun = carried_guns[gun].instance()
+	$Pivot/Gun.add_child(new_gun)
+	PlayerStats.current_gun = new_gun.name
+
 	
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("next_gun"):
+		current_gun+=1
+		if current_gun > len(carried_guns)-1:
+			current_gun = 0 
+		change_gun(current_gun)
+	elif Input.is_action_just_pressed("prev_gun"):
+		current_gun -=1
+		if current_gun <0:
+			current_gun = len(carried_guns)-1
+		change_gun(current_gun)
 	
 	
 	
